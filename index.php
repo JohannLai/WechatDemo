665a7fe9df00929862cdd1ab1695f51c5cbaa0cd
JohannLai <johann_lai@outlook.com>
2016-03-31 20:41:29 +0800

    Add TOKEN test

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once "send_text_msg.php";
 //require_once "downloadfile.php";
 
 
-    
-    
+
+
 $make_menu_url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".ACCESS_TOKEN ;
 
 
@@ -13,51 +13,51 @@ $menudata = ' {
 
     "button": [
         {
-            "name": "扫码", 
+            "name": "扫码",
             "sub_button": [
                 {
-                    "type": "scancode_waitmsg", 
-                    "name": "扫码带提示", 
-                    "key": "rselfmenu_0_0", 
+                    "type": "scancode_waitmsg",
+                    "name": "扫码带提示",
+                    "key": "rselfmenu_0_0",
                     "sub_button": [ ]
-                }, 
+                },
                 {
-                    "type": "scancode_push", 
-                    "name": "扫码推事件", 
-                    "key": "rselfmenu_0_1", 
+                    "type": "scancode_push",
+                    "name": "扫码推事件",
+                    "key": "rselfmenu_0_1",
                     "sub_button": [ ]
                 }
             ]
-        }, 
+        },
         {
-            "name": "发图", 
+            "name": "发图",
             "sub_button": [
                 {
-                    "type": "pic_sysphoto", 
-                    "name": "系统拍照发图", 
-                    "key": "rselfmenu_1_0", 
+                    "type": "pic_sysphoto",
+                    "name": "系统拍照发图",
+                    "key": "rselfmenu_1_0",
                     "sub_button": [ ]
-                }, 
+                },
                 {
-                    "type": "pic_photo_or_album", 
-                    "name": "拍照或者相册发图", 
-                    "key": "rselfmenu_1_1", 
+                    "type": "pic_photo_or_album",
+                    "name": "拍照或者相册发图",
+                    "key": "rselfmenu_1_1",
                     "sub_button": [ ]
-                }, 
+                },
                 {
-                    "type": "pic_weixin", 
-                    "name": "微信相册发图", 
-                    "key": "rselfmenu_1_2", 
+                    "type": "pic_weixin",
+                    "name": "微信相册发图",
+                    "key": "rselfmenu_1_2",
                     "sub_button": [ ]
                 }
             ]
-        }, 
+        },
         {
-            "name": "发送位置", 
-            "type": "location_select", 
+            "name": "发送位置",
+            "type": "location_select",
             "key": "rselfmenu_2_0"
-        }, 
-        
+        },
+
     ]
 
 
--- send_text_msg.php
+++ send_text_msg.php
@@ -18,73 +18,73 @@ class wechatCallbackapiTest
     }
     public function responseMsg()
     {
-	
+
       $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];//获取微信服务器提交过来的数据
-       
+
       $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);//将获取的处理处理
-      
+
       $fromUsername = $postObj->FromUserName;  //用户的openid ，不是唯一的，相同的用户关注不同的公众号后，获取的openid是不同的
       $toUsername = $postObj->ToUserName; //发送方微信号（来自用户 故为openid）
-        
+
       $get_msg_by_openid = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".ACCESS_TOKEN."&openid=".$fromUsername."&lang=zh_CN";
-        
-       $result_json = file_get_contents( $get_msg_by_openid ); 
-       
+
+       $result_json = file_get_contents( $get_msg_by_openid );
+
        $user_msg = json_decode($result_json, true);
-      
+
       $keyword = trim($postObj->Content); //用户发送的消息内容
       $MsgType = $postObj->MsgType; //消息的类型为"text"，微信会根据用户发送不同消息自动识别返回给BAE不同值(text, image, event, voice, video,location)
-      
-      $Ticket = $postObj->Ticket;
-        
-      $Event = $postObj->Event;
-        
-      $EventKey = $postObj->EventKey;  
 
-      $time = time(); //回复消息的时间戳  
-        
+      $Ticket = $postObj->Ticket;
+
+      $Event = $postObj->Event;
+
+      $EventKey = $postObj->EventKey;
+
+      $time = time(); //回复消息的时间戳
+
        $subscribe_time = date("Y-m-d H:i:s",  $user_msg['subscribe_time']);
-        
+
       $msg = "关注用户：". $user_msg['nickname'] ."\n";
        $msg .= "关注时间：". $subscribe_time ."\n";
        $msg .= "所在地：". $user_msg['country'] . $user_msg['province'] .$user_msg['city']."\n";
-       $msg .= "头像链接：".$user_msg['headimgurl'] ."\n"; 
+       $msg .= "头像链接：".$user_msg['headimgurl'] ."\n";
       $msg .= "用户id：".$fromUsername."\n";
       $msg .= "消息类型：".$MsgType."\n";
       $msg .= "事件类型：".$Event."\n";
       $msg .= "事件的key值：".$EventKey."\n";
       $msg .= "当前扫描二维码的ticket值：".$Ticket."\n";
-      $msg .= "事件发送过来的时间戳：".$time."\n";  
-        
+      $msg .= "事件发送过来的时间戳：".$time."\n";
+
       $textTpl = "<xml>
 					<ToUserName><![CDATA[%s]]></ToUserName>
 					<FromUserName><![CDATA[%s]]></FromUserName>
 					<CreateTime>%s</CreateTime>
 					<MsgType><![CDATA[%s]]></MsgType>
 					<Content><![CDATA[%s]]></Content>
-			</xml>"; 
-          
+			</xml>";
+
       $msgType = "text"; //回复消息的类型
       $contentStr = $msg; //回复给微信用户的内容
-      
+
       $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);//经过sprintf处理过后，
       echo $resultStr;
       exit;
 
     }
-		
+
 	private function checkSignature()
 	{
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-        		
+        $nonce = $_GET["nonce"];
+
 		$token = TOKEN;
 		$tmpArr = array($token, $timestamp, $nonce);
 		sort($tmpArr);
 		$tmpStr = implode( $tmpArr );
 		$tmpStr = sha1( $tmpStr );
-		
+
 		if( $tmpStr == $signature ){
 			return true;
 		}else{
